b100e3025c16457789dfe323bb99a6396ec4aa01
Roman Havrylenko <g4vr0m@gmail.com>
2022-06-10 12:30:40 +0300

    add async custom icons

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,4 +1,18 @@
 import moment from 'moment';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBulz7y3vD9x1CMeSpWrof1oXEPqS0JT94',
+    authDomain: 'weather-app-df5ba.firebaseapp.com',
+    projectId: 'weather-app-df5ba',
+    storageBucket: 'weather-app-df5ba.appspot.com',
+    messagingSenderId: '959370567245',
+    appId: '1:959370567245:web:38e0fa9d5504611b01c604',
+    measurementId: 'G-47QHNBZ6CY',
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
 class Weather {
     URL = 'https://api.openweathermap.org/data/2.5/onecall';
@@ -15,46 +29,44 @@ class Weather {
         });
     };
 
-    renderCurrent = (current, element) => {
+    async renderCurrent(current, element) {
         element.innerHTML = '';
 
         const { dt, temp, weather } = current;
+
+        const iconUrl = await this.getIconUrl(weather[0].icon);
         const markup = `
             <p>${moment.unix(dt).format('dddd, DD MMMM')}</p>
             <p>${Math.round(temp)}°</p>
-
-            <img src="http://openweathermap.org/img/w/${
-                weather[0].icon
-            }.png" class="weather-current-icon">
+            <img src="${iconUrl}" class="weather-current-icon">
             <p>${weather[0].description}</p>
             `;
 
         element.insertAdjacentHTML('beforeend', markup);
-    };
+    }
 
-    renderForecast = (daily, element) => {
+    async renderForecast(daily, element) {
         element.innerHTML = '';
 
-        const markup = daily
-            .filter((el, idx) => idx !== 0)
-            .map(
-                ({ dt, temp, weather }) => `
+        const week = daily.filter((el, idx) => idx !== 0);
+
+        const markupArr = await Promise.all(
+            week.map(async ({ dt, temp, weather }) => {
+                const iconUrl = await this.getIconUrl(weather[0].icon);
+                return `
                     <li class="weather-daily-item">
                         <p>${moment.unix(dt).format('dddd')}</p>
                         <p>${Math.round(temp.min)}° - ${Math.round(temp.max)}°</p>
                         <div class="weather-icon-wrap">
-                        
-                            <img src="http://openweathermap.org/img/w/${
-                                weather[0].icon
-                            }.png" class="weather-daily-icon">
+                            <img src="${iconUrl}.svg" class="weather-daily-icon">
                         </div>
                     </li>
-    `,
-            )
-            .join('');
-
+                `;
+            }),
+        );
+        const markup = markupArr.join('');
         element.insertAdjacentHTML('beforeend', markup);
-    };
+    }
 
     renderHighlights = (current, daily, element) => {
         element.innerHTML = '';
@@ -94,6 +106,13 @@ class Weather {
             `;
 
         element.insertAdjacentHTML('beforeend', markup);
+    };
+
+    getIconUrl = id => {
+        const iconId = `${id}.svg`;
+        return getDownloadURL(ref(storage, iconId)).then(url => {
+            return url;
+        });
     };
 }
 
